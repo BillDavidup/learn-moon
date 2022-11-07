7d2c2e81a0cd1ee1e3e9e8bc28585493807b56db
BillDavidup <BillDavidup@hotmail.com>
2022-11-07 23:09:10 +0800

    项目：删除无效import

--- learn-moon-web/src/main/java/com/ning/learn/moon/controller/ServiceAliveController.java
+++ learn-moon-web/src/main/java/com/ning/learn/moon/controller/ServiceAliveController.java
@@ -1,10 +1,8 @@
 package com.ning.learn.moon.controller;
 
 import com.ning.learn.moon.common.result.Result;
-import com.ning.learn.moon.userinfo.bo.UserInfoBO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
